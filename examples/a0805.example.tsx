b2536367ba54d010aa4a73d1aa00ce8fb3a69cbd
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2024-07-23 17:41:30 +0300

    implement 0805 component

---
+++ examples/a0805.example.tsx
@@ -0,0 +1,12 @@
+import { JsCadFixture } from "jscad-fiber"
+import { A0805 } from "../lib/A0805"
+import { ExtrudedPads } from "../src/lib/ExtrudedPads"
+
+export default () => {
+  return (
+    <JsCadFixture>
+      <A0805 />
+      <ExtrudedPads footprint="0805" />
+    </JsCadFixture>
+  )
+}
