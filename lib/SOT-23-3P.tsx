e8a32feed11a3f4673485a6c5c89944e96916461
Abhijit Roy <roya51788@gmail.com>
2024-08-24 00:34:37 +0530

    fix:SOT-23-3P lead positions

--- lib/SOT-23-3P.tsx
+++ lib/SOT-23-3P.tsx
@@ -1,68 +1,68 @@
 import { ChipBody } from "./ChipBody";
 import { SmdChipLead } from "./SmdChipLead";
 
-export const SOT233P = () => {
-  const fullWidth = 1.25;
-  const fullLength = 2.92;
+export const SOT233P = ({ fullWidth = 2.9, fullLength = 2.8 }) => {
+  const bodyWidth = 1.3;
+  const bodyLength = 2.9;
+  const bodyHeight = 1.1;
+  const leadWidth = 0.4;
+  const leadThickness = 0.15;
+  const leadHeight = 0.95;
+  const padContactLength = 0.6;
 
   return (
     <>
-      {/* Lead 1 */}
+      {/* Leads on the right side (pins 1 and 2) */}
       <SmdChipLead
         key={1}
+        rotation={Math.PI}
         position={{
-          x: -fullWidth / 2,
+          x: fullWidth / 2,
           y: 0,
-          z: 0,
+          z: -0.95,
         }}
-        width={0.25}
-        thickness={0.15}
-        padContactLength={0.6}
-        bodyDistance={(1.25 - 0.5) / 2}
-        height={0.55}
-        curveLength={0.3}
+        width={leadWidth}
+        thickness={leadThickness}
+        padContactLength={padContactLength}
+        bodyDistance={(fullWidth - bodyWidth) / 2}
+        height={leadHeight}
       />
-
-      {/* Lead 2 */}
       <SmdChipLead
         key={2}
         rotation={Math.PI}
         position={{
           x: fullWidth / 2,
           y: 0,
-          z: -fullLength / 2 + 0.95,
+          z: 0.95,
         }}
-        width={0.25}
-        thickness={0.15}
-        padContactLength={0.6}
-        bodyDistance={(1.25 - 0.5) / 2}
-        height={0.55}
-        curveLength={0.3}
+        width={leadWidth}
+        thickness={leadThickness}
+        padContactLength={padContactLength}
+        bodyDistance={(fullWidth - bodyWidth) / 2}
+        height={leadHeight}
       />
 
-      {/* Lead 3 */}
+      {/* Lead on the left side (pin 3) */}
       <SmdChipLead
         key={3}
-        rotation={Math.PI}
         position={{
-          x: fullWidth / 2,
+          x: -fullWidth / 2,
           y: 0,
-          z: fullLength / 2 - 0.95,
+          z: 0,
         }}
-        width={0.25}
-        thickness={0.15}
-        padContactLength={0.6}
-        bodyDistance={(1.25 - 0.5) / 2}
-        height={0.55}
-        curveLength={0.3}
+        width={leadWidth}
+        thickness={leadThickness}
+        padContactLength={padContactLength}
+        bodyDistance={(fullWidth - bodyWidth) / 2}
+        height={leadHeight}
       />
 
       {/* Chip Body */}
       <ChipBody
-        center={{ x: 0, y: 0, z: 0 }}
-        width={fullWidth}
-        length={fullLength}
-        height={1.52}
+        center={{ x: 0, y: bodyHeight / 2, z: 0 }}
+        width={bodyWidth}
+        length={bodyLength}
+        height={bodyHeight}
       />
     </>
   );
