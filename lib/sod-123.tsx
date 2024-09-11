097c7e96d57f1733968cd80666bbbb9550c5eac8
Abhijit Roy <roya51788@gmail.com>
2024-09-12 00:58:59 +0530

    adjust dimensions

--- lib/sod-123.tsx
+++ lib/sod-123.tsx
@@ -1,16 +1,16 @@
 import { ChipBody } from "./ChipBody";
 import { SmdChipLead } from "./SmdChipLead";
 
-export const SOD123 = ({ fullWidth = 4.5, fullLength = 3.0 }) => {
+export const SOD123 = ({ fullWidth = 3.8, fullLength = 1.6 }) => {
   const bodyWidth = 2.9;
   const bodyLength = 1.3;
   const bodyHeight = 1.1;
-  const leadWidth = 0.4;
+  const leadWidth = 0.8;
   const leadThickness = 0.15;
-  const leadHeight = 0.95;
-  const padContactLength = 0.6;
-  const leadYOffset = 0.1;
-  const bodyYOffset = -0.11;
+  const leadHeight = 0.5;
+  const padContactLength = 0.4;
+  const leadYOffset = leadHeight / 1 - 0.4
+  const bodyYOffset = (leadHeight / 2) - 0.4;
 
   const bodyDistance = (fullWidth - bodyWidth) / 2;
 
@@ -49,7 +49,7 @@ export const SOD123 = ({ fullWidth = 4.5, fullLength = 3.0 }) => {
 
       {/* Chip Body */}
       <ChipBody
-        center={{ x: 0, y: bodyHeight / 2 + bodyYOffset, z: 0 }}
+        center={{ x: 0, y: bodyYOffset, z: 0 }}
         width={bodyWidth}
         length={bodyLength}
         height={bodyHeight}
