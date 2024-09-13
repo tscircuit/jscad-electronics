8a10d66a8be879a84102f7d84e169f51809e5208
Abhijit Roy <roya51788@gmail.com>
2024-09-13 14:00:16 +0530

    Adjust dimensions and fix spacing

--- lib/SOT-563.tsx
+++ lib/SOT-563.tsx
@@ -1,42 +1,46 @@
-import React from 'react';
-import { Cuboid, Translate, Rotate } from 'jscad-fiber';
+import { Cuboid, Translate, Rotate, Colorize } from "jscad-fiber"
 
-export const SOT563 = ({ fullWidth = 1.6, fullLength = 1.6 }) => {
- 
-  const bodyWidth = 1.6;
-  const bodyLength = 1.6;
-  const bodyHeight = 0.55;
-  const leadWidth = 0.2;
-  const leadLength = 0.3;
-  const leadHeight = 0.1;
-  const leadSpacing = 0.5;
-  const bodyYOffset = leadHeight;
+export const SOT563 = ({ fullWidth = 2.0, fullLength = 1.6 }) => {
+  const bodyWidth = 1.6
+  const bodyLength = 1.6
+  const bodyHeight = 0.55
+  const leadWidth = 0.3
+  const leadLength = 0.2
+  const leadHeight = 0.1
+  const leadSpacing = 0.5
+  const bodyYOffset = leadHeight / 2
+  const bodyZOffset = -0.3
 
   return (
     <>
       {/* Main body - rotated 90 degrees around X-axis */}
       <Rotate rotation={[(90 / 180) * Math.PI, 0, 0]}>
-        <Translate center={[0, bodyYOffset, 0]}>
-          <Cuboid size={[bodyWidth, bodyLength, bodyHeight]} />
+        <Translate center={[0, bodyYOffset, bodyZOffset]}>
+          <Colorize color="grey">
+            <Cuboid size={[bodyWidth, bodyLength, bodyHeight]} />
+          </Colorize>
         </Translate>
       </Rotate>
 
       {/* Leads */}
-      {[-1, 0, 1].map((xOffset, index) => (
-        <React.Fragment key={index}>
-          {/* Top lead */}
-          <Translate center={[xOffset * leadSpacing, 0, fullLength / 2 - leadLength / 2]}>
-            <Cuboid size={[leadWidth, leadHeight, leadLength]} />
-          </Translate>
-          
-          {/* Bottom lead */}
-          <Translate center={[xOffset * leadSpacing, 0, -fullLength / 2 + leadLength / 2]}>
-            <Cuboid size={[leadWidth, leadHeight, leadLength]} />
-          </Translate>
-        </React.Fragment>
-      ))}
+      {[-1, 0, 1].flatMap((yOffset, index) => [
+        // Left lead
+        <Translate
+          key={`left-${index}`}
+          center={[-fullWidth / 2 + leadWidth / 2, 0, yOffset * leadSpacing]}
+        >
+          <Cuboid size={[leadWidth, leadHeight, leadLength]} />
+        </Translate>,
+        // Right lead
+        <Translate
+          key={`right-${index}`}
+          center={[fullWidth / 2 - leadWidth / 2, 0, yOffset * leadSpacing]}
+        >
+          <Cuboid size={[leadWidth, leadHeight, leadLength]} />
+        </Translate>
+      ])}
     </>
   );
 };
 
-export default SOT563;
+export default SOT563
