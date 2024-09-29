70ee029fe692a452ce14e013722407a268d71df4
Abhijit Roy <roya51788@gmail.com>
2024-09-29 23:23:44 +0530

    fix: bga to not be on side

--- lib/BGA.tsx
+++ lib/BGA.tsx
@@ -28,8 +28,8 @@ export const BGA = ({
   footprintString,
 }: BGAProps) => {
   const bodyHeight = packageHeight - standoffHeight;
-  const bodyOffset = 1.4;
-  const ballOffset = 0.1;
+  const bodyOffset = standoffHeight + bodyHeight / 2;
+  const ballOffset = -standoffHeight / 2;
 
   const ballsSoup = useMemo(() => {
     if (!footprintString) return null;
@@ -40,49 +40,37 @@ export const BGA = ({
   return (
     <>
       {/* Package body */}
+      <Translate z={bodyOffset}>
       <Colorize color="#555">
-        <Rotate rotation={[(90 / 180) * Math.PI, 0, 0]}>
-          <Translate center={[0, 0, -bodyOffset]}>
-            <Cuboid
-              size={[packageWidth, packageLength, bodyHeight]}
-              center={[0, 0, standoffHeight + bodyHeight / 2]}
-            />
-          </Translate>
-        </Rotate>
-      </Colorize>
+        <Cuboid
+          size={[packageWidth, packageLength, bodyHeight]}
+        />
+        </Colorize>
+      </Translate>
 
       {/* Balls via ball parameters */}
       {!footprintString &&
         Array.from({ length: ballRows * ballColumns }).map((_, index) => {
           if (missingBalls.includes(index + 1)) return null;
-
           const row = Math.floor(index / ballColumns);
           const col = index % ballColumns;
           const x = (col - (ballColumns - 1) / 2) * ballPitch;
           const y = (row - (ballRows - 1) / 2) * ballPitch;
-
           return (
-            <Translate center={[x, y, standoffHeight / 2 - ballOffset]}>
-              <Sphere radius={ballDiameter / 2} />
+            <Translate key={index} x={x} y={y} z={ballOffset}>
+                <Sphere radius={ballDiameter / 2} />
             </Translate>
           );
         })}
 
       {/* Balls via footprint string */}
       {ballsSoup &&
-        ballsSoup.map((elm) => {
+        ballsSoup.map((elm, index) => {
           if (elm.type === "pcb_smtpad") {
             return (
-              <Rotate
-                key={elm.pcb_smtpad_id}
-                rotation={[(90 / 180) * Math.PI, 0, 0]}
-              >
-                <Translate
-                  center={[elm.x, elm.y, standoffHeight / 2 - ballOffset]}
-                >
+              <Translate key={index} x={elm.x} y={elm.y} z={ballOffset}>
                   <Sphere radius={ballDiameter / 2} />
-                </Translate>
-              </Rotate>
+              </Translate>
             );
           }
           return null;
