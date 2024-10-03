e065d7796b7321079c7a53e9abd9ef341be9798c
Anas sarkiz <anassoftwaredev@gmail.com>
2024-10-03 20:06:22 +0200

    formatted

--- examples/TSSOP-20.example.tsx
+++ examples/TSSOP-20.example.tsx
@@ -5,9 +5,9 @@ import { Footprinter3d } from "lib/Footprinter3d"
 export default () => {
   const fp = "tssop20_w4.5mm_p0.65mm_pl0.6mm_pw0.2mm"
   return (
-    < JsCadFixture zAxisUp>
+    <JsCadFixture zAxisUp>
       <Footprinter3d footprint={fp} />
       <ExtrudedPads footprint={fp} />
-    </JsCadFixture >
+    </JsCadFixture>
   )
 }
--- lib/Tssop.tsx
+++ lib/Tssop.tsx
@@ -1,4 +1,4 @@
-import { fp } from "@tscircuit/footprinter";
+import { fp } from "@tscircuit/footprinter"
 import { ChipBody } from "./ChipBody"
 import { SmdChipLead } from "./SmdChipLead"
 
@@ -8,9 +8,8 @@ export const Tssop = ({
   fpJson,
   fullWidth = 6.4,
 }: { pinCount?: number; fullWidth?: number; fpJson: any }) => {
-
   const sidePinLength = Math.ceil(fpJson.num_pins / 2)
-  const fullLength = fpJson.p * fpJson.num_pins / 2 + (fpJson.pw / 2)
+  const fullLength = (fpJson.p * fpJson.num_pins) / 2 + fpJson.pw / 2
   const pinOffsetToCenter = ((sidePinLength - 1) * fpJson.p) / 2
   return (
     <>
@@ -36,7 +35,7 @@ export const Tssop = ({
           position={{
             x: fpJson.w / 2 + fpJson.pl * 2,
             y: i * fpJson.p - pinOffsetToCenter,
-            z: 0
+            z: 0,
           }}
           width={fpJson.pw}
           thickness={0.25}
