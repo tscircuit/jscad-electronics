2544b39b07971c6ef9ac49ff0c6a09c5635a7f88
Priyanshu1035 <priyanshukhawas18@gmail.com>
2024-10-09 11:11:29 +0530

    addedpins

--- lib/PushButton.tsx
+++ lib/PushButton.tsx
@@ -7,6 +7,9 @@ const baseLength = 0.8
 const baseWidth = 0.8
 const baseHeight = 0.1
 
+const pinRadius = 0.05
+const pinHeight = 0.1
+
 export const PushButton = () => {
   return (
     <>
@@ -21,6 +24,31 @@ export const PushButton = () => {
         size={[baseLength, baseWidth, baseHeight]}
         offset={[0, 0, baseHeight / 2]}
         color="#ccc"
+      />
+
+      <Cylinder
+        radius={pinRadius}
+        height={pinHeight}
+        offset={[-baseLength / 2 + pinRadius, -baseWidth / 2 + pinRadius, 0]}
+        color="#000"
+      />
+      <Cylinder
+        radius={pinRadius}
+        height={pinHeight}
+        offset={[baseLength / 2 - pinRadius, -baseWidth / 2 + pinRadius, 0]}
+        color="#000"
+      />
+      <Cylinder
+        radius={pinRadius}
+        height={pinHeight}
+        offset={[-baseLength / 2 + pinRadius, baseWidth / 2 - pinRadius, 0]}
+        color="#000"
+      />
+      <Cylinder
+        radius={pinRadius}
+        height={pinHeight}
+        offset={[baseLength / 2 - pinRadius, baseWidth / 2 - pinRadius, 0]}
+        color="#000"
       />
     </>
   )
