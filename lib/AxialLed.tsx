a0618edb5c34cc3ed66742cdaf06facc0a19e4e4
alhamramzrn <alhamramzrn@gmail.com>
2024-10-11 06:20:20 +0000

    fix format

--- lib/AxialLed.tsx
+++ lib/AxialLed.tsx
@@ -1,5 +1,5 @@
-import { Cylinder, Cuboid, Sphere } from "jscad-fiber"
 import { mm } from "@tscircuit/mm"
+import { Cuboid, Cylinder, Sphere } from "jscad-fiber"
 
 interface AxialLedProps {
   pitch: number
@@ -19,8 +19,17 @@ export const AxialLed = ({ pitch = 10 }: AxialLedProps) => {
         rotation={[0, "90deg", 0]}
       />
 
-      <Cuboid size={[2.5, 2.5, 1]} center={[0, 0, heightToCenterOfLed]} color="red" />
-      <Cylinder height={2} radius={1} center={[0, 0, heightToCenterOfLed + 1]} color="red" />
+      <Cuboid
+        size={[2.5, 2.5, 1]}
+        center={[0, 0, heightToCenterOfLed]}
+        color="red"
+      />
+      <Cylinder
+        height={2}
+        radius={1}
+        center={[0, 0, heightToCenterOfLed + 1]}
+        color="red"
+      />
       <Sphere radius={1} center={[0, 0, heightToCenterOfLed + 2]} color="red" />
 
       <Cylinder height={4} radius={0.5} center={[pitch / 2, 0, 0.5]} />
