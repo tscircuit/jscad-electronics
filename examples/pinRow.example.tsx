5c6d81c9ed008f568eadedb587f25146cc8f05d4
Anas sarkiz <anassoftwaredev@gmail.com>
2024-10-18 15:28:06 +0200

    removed console log

--- examples/pinRow.example.tsx
+++ examples/pinRow.example.tsx
@@ -1,9 +1,7 @@
 import { JsCadFixture } from "jscad-fiber"
 import { ExtrudedPads } from "lib/index"
 import { PinRow } from "lib/Pinrow"
-import { fp } from "@tscircuit/footprinter/dist/index"
 export default () => {
-  console.log(fp.string("pinrow10").json())
   return (
     <JsCadFixture zAxisUp showGrid>
       <ExtrudedPads footprint="pinrow10_id0.3mm_p1mm_od0.4mm" />
