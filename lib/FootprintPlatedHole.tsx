54bbc565ae5fe9d960d465e30dbd8336bcea43e6
Rishabh Gupta <belikerishabh@gmail.com>
2025-02-15 18:04:48 +0530

    fix: dip plated_hole placement fixed

--- lib/FootprintPlatedHole.tsx
+++ lib/FootprintPlatedHole.tsx
@@ -5,7 +5,8 @@ export const FootprintPlatedHole = ({ hole }: { hole: PcbPlatedHole }) => {
   if (hole.shape === "circle") {
     return (
       <Colorize color="#b87333">
-        <Translate offset={[hole.x, hole.y, 0]}>
+        {/* Z index to move the footprint below the extruded pads */}
+        <Translate offset={[hole.x, hole.y, -0.3]}>
           {/* <Rotate axis="z" angle={90}> */}
           <Subtract>
             <Cylinder radius={hole.outer_diameter / 2} height={0.01} />
