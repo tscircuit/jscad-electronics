b95bd4af454f222548ad41851cedfdd75fe29a49
Anas sarkiz <72264380+AnasSarkiz@users.noreply.github.com>
2025-02-21 19:23:22 +0200

    Introduce Female headers 3d model and added example (#89)
    * Introduce Female headers 3d model and added example
    
    * formatted
    
    * Added pinrow-female in footprinter3d

---
+++ examples/femaleHeader.example.tsx
@@ -0,0 +1,11 @@
+import { FemaleHeader } from "lib/FemaleHeader"
+import { JsCadView } from "jscad-fiber"
+import { ExtrudedPads } from "lib/index"
+export default () => {
+  return (
+    <JsCadView zAxisUp showGrid>
+      <ExtrudedPads footprint="pinrow5_id01mm_p2.54mm_od01.6mm" />
+      <FemaleHeader numberOfPins={5} legsLength={3} pitch={2.54} />
+    </JsCadView>
+  )
+}
---
+++ examples/footprinter3d/fp-pinrow6-female.example.tsx
@@ -0,0 +1,13 @@
+import { JsCadView } from "jscad-fiber"
+import { Footprinter3d } from "lib/Footprinter3d"
+import { ExtrudedPads } from "lib/ExtrudedPads"
+
+const footprint = "pinrow6_id01mm_p2.54mm_od01.6mm_female"
+export default () => {
+  return (
+    <JsCadView zAxisUp showGrid>
+      <Footprinter3d footprint={footprint} />
+      <ExtrudedPads footprint={footprint} />
+    </JsCadView>
+  )
+}
