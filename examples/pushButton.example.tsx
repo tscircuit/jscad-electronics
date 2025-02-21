968956cc7afeae81ce494291659cac249c033e03
Anas sarkiz <anassoftwaredev@gmail.com>
2025-02-21 20:42:19 +0200

    Introduce pushButton 3d component

---
+++ examples/pushButton.example.tsx
@@ -0,0 +1,11 @@
+import { JsCadView } from "jscad-fiber"
+import { PushButton } from "lib/PushButton"
+import { ExtrudedPads } from "../lib/ExtrudedPads"
+export default () => {
+  return (
+    <JsCadView zAxisUp showGrid>
+      <PushButton width={4.5} length={6.5} innerDiameter={1} />
+      <ExtrudedPads footprint="pushbutton" />
+    </JsCadView>
+  )
+}
