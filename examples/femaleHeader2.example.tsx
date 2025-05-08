ce7b3cb910c046ee9b591d2f86a91c15443993e1
Shibo <shibosoftwaredev@gmail.com>
2025-05-08 19:04:13 +0200

    pin header outerdiameter and innerdiameter

---
+++ examples/femaleHeader2.example.tsx
@@ -0,0 +1,17 @@
+import { FemaleHeader } from "lib/FemaleHeader"
+import { JsCadView } from "jscad-fiber"
+import { ExtrudedPads } from "lib/index"
+export default () => {
+  return (
+    <JsCadView zAxisUp showGrid>
+      <ExtrudedPads footprint="pinrow5_id01mm_p4mm_od4mm_id2mm" />
+      <FemaleHeader
+        numberOfPins={5}
+        legsLength={3}
+        innerDiameter={2}
+        outerDiameter={4}
+        pitch={4}
+      />
+    </JsCadView>
+  )
+}
--- lib/FemaleHeader.tsx
+++ lib/FemaleHeader.tsx
@@ -1,17 +1,22 @@
-import { Cuboid, Colorize, Hull, Subtract } from "jscad-fiber"
+import { Cuboid, Colorize, Hull, Subtract, Cylinder } from "jscad-fiber"
 
 export const FemaleHeader = ({
   numberOfPins,
   pitch = 2.54,
   legsLength = 3,
+  outerDiameter = 0.945,
+  innerDiameter = 0.945,
 }: {
   numberOfPins: number
   pitch?: number
   legsLength?: number
+  outerDiameter?: number
+  innerDiameter?: number
 }) => {
-  const pinThickness = 0.63
+  const pinThickness = innerDiameter / 1.5
+  const bodyDepth = pinThickness * 2 + outerDiameter
   const bodyHeight = 5
-  const bodyWidth = numberOfPins * pitch
+  const bodyWidth = (numberOfPins - 1) * pitch + outerDiameter + pitch / 2
   const gapWidth = pinThickness * 1.6
   const xoff = -((numberOfPins - 1) / 2) * pitch
   const Body = (
@@ -19,15 +24,26 @@ export const FemaleHeader = ({
       <Subtract>
         <Cuboid
           color="#000"
-          size={[bodyWidth, pinThickness * 3, bodyHeight]}
+          size={[bodyWidth, bodyDepth, bodyHeight]}
           center={[0, 0, bodyHeight / 2]}
         />
-        {Array.from({ length: numberOfPins }, (_, i) => (
-          <Cuboid
-            size={[gapWidth, gapWidth, bodyHeight]}
-            center={[xoff + i * pitch, 0, bodyHeight / 2]}
-          />
-        ))}
+        {Array.from({ length: numberOfPins }, (_, i) =>
+          innerDiameter ? (
+            <Cylinder
+              key={i}
+              height={bodyHeight + 0.1}
+              radius={innerDiameter / 2}
+              center={[xoff + i * pitch, 0, bodyHeight / 2]}
+              color="#222"
+            />
+          ) : (
+            <Cuboid
+              key={i}
+              size={[gapWidth, gapWidth, bodyHeight]}
+              center={[xoff + i * pitch, 0, bodyHeight / 2]}
+            />
+          ),
+        )}
       </Subtract>
     </Colorize>
   )
@@ -35,28 +51,25 @@ export const FemaleHeader = ({
     <>
       {Body}
       {Array.from({ length: numberOfPins }, (_, i) => (
-        <>
-          {/*Long pins (bottom) */}
-          <Colorize color="silver">
-            <Hull>
-              <Cuboid
-                color="silver"
-                size={[pinThickness, pinThickness, legsLength * 0.9]}
-                center={[xoff + i * pitch, 0, (-legsLength / 2) * 0.9]}
-              />
-              <Cuboid
-                color="silver"
-                size={[pinThickness / 1.8, pinThickness / 1.8, legsLength]}
-                center={[xoff + i * pitch, 0, -legsLength / 2]}
-              />
-            </Hull>
+        <Colorize color="silver" key={i}>
+          <Hull>
             <Cuboid
               color="silver"
-              size={[gapWidth, gapWidth, gapWidth * 0.5]}
-              center={[xoff + i * pitch, 0, (gapWidth / 2) * 0.5]}
+              size={[pinThickness, pinThickness, legsLength * 0.9]}
+              center={[xoff + i * pitch, 0, (-legsLength / 2) * 0.9]}
             />
-          </Colorize>
-        </>
+            <Cuboid
+              color="silver"
+              size={[pinThickness / 1.8, pinThickness / 1.8, legsLength]}
+              center={[xoff + i * pitch, 0, -legsLength / 2]}
+            />
+          </Hull>
+          <Cuboid
+            color="silver"
+            size={[gapWidth, gapWidth, gapWidth * 0.5]}
+            center={[xoff + i * pitch, 0, (gapWidth / 2) * 0.5]}
+          />
+        </Colorize>
       ))}
     </>
   )
