4299de1aa2fb8a524add7730e5f56f5e5a1a9b01
Shibo <shibosoftwaredev@gmail.com>
2025-05-08 19:57:28 +0200

    fixed pitch

--- examples/femaleHeader2.example.tsx
+++ examples/femaleHeader2.example.tsx
@@ -4,13 +4,13 @@ import { ExtrudedPads } from "lib/index"
 export default () => {
   return (
     <JsCadView zAxisUp showGrid>
-      <ExtrudedPads footprint="pinrow5_id01mm_p4mm_od4mm_id2mm" />
+      <ExtrudedPads footprint="pinrow5_id01mm_p8mm_od4mm_id2mm" />
       <FemaleHeader
         numberOfPins={5}
         legsLength={3}
         innerDiameter={2}
         outerDiameter={4}
-        pitch={4}
+        pitch={8}
       />
     </JsCadView>
   )
