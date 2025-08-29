76e05fa2ade6fb797e6acab6ce615ed45841796b
Shibo <shibosoftwaredev@gmail.com>
2025-08-29 17:21:05 +0200

    implement createJSCADRenderer without using react or it's reconciler (#109)
    * implement createJSCADRenderer without using react or it's reconciler
    
    * implement convertCSGToThreeGeom

---
+++ lib/vanilla/convertCSGToThreeGeom.ts
@@ -0,0 +1,101 @@
+import {
+  BufferAttribute,
+  BufferGeometry,
+  Color,
+  Matrix4,
+  Vector2,
+  Vector3,
+} from "three"
+
+export function convertCSGToThreeGeom(csg: any): BufferGeometry {
+  if (csg.polygons) {
+    // 3D shape
+    const vertices = []
+    const indices = []
+    const colors = []
+    let idx = 0
+
+    for (const polygon of csg.polygons) {
+      for (const vertex of polygon.vertices) {
+        vertex.index = idx
+        vertices.push(vertex[0], vertex[1], vertex[2])
+        // Add color for each vertex
+        if (csg.color && csg.color.length >= 3) {
+          colors.push(csg.color[0], csg.color[1], csg.color[2])
+        } else {
+          colors.push(1, 1, 1) // Default to white if no color
+        }
+        idx++
+      }
+      const first = polygon.vertices[0].index
+      for (let i = 2; i < polygon.vertices.length; i++) {
+        const second = polygon.vertices[i - 1].index
+        const third = polygon.vertices[i].index
+        indices.push(first, second, third)
+      }
+    }
+
+    const geo = new BufferGeometry()
+    geo.setAttribute(
+      "position",
+      new BufferAttribute(new Float32Array(vertices), 3),
+    )
+    geo.setIndex(indices)
+
+    // Add color attribute
+    if (colors.length > 0) {
+      geo.setAttribute(
+        "color",
+        new BufferAttribute(new Float32Array(colors), 3),
+      )
+    }
+
+    if (csg.transforms) {
+      const transforms = new Matrix4()
+      transforms.fromArray(csg.transforms)
+      geo.applyMatrix4(transforms)
+    }
+    geo.computeVertexNormals()
+
+    return geo
+  }
+  if (csg.sides) {
+    // 2D shape
+    const vertices = []
+    const colors = []
+
+    for (const side of csg.sides) {
+      vertices.push(side[0][0], side[0][1], 0) // Add z-coordinate as 0
+      // Add color for each vertex
+      if (csg.color && csg.color.length >= 3) {
+        colors.push(csg.color[0], csg.color[1], csg.color[2])
+      } else {
+        colors.push(1, 1, 1) // Default to white if no color
+      }
+    }
+
+    const geo = new BufferGeometry()
+    geo.setAttribute(
+      "position",
+      new BufferAttribute(new Float32Array(vertices), 3),
+    )
+
+    // Add color attribute
+    if (colors.length > 0) {
+      geo.setAttribute(
+        "color",
+        new BufferAttribute(new Float32Array(colors), 3),
+      )
+    }
+
+    if (csg.transforms) {
+      const transforms = new Matrix4()
+      transforms.fromArray(csg.transforms)
+      geo.applyMatrix4(transforms)
+    }
+
+    return geo
+  }
+  console.error("Invalid CSG object: neither polygons nor sides found")
+  return new BufferGeometry()
+}
--- lib/vanilla/index.ts
+++ lib/vanilla/index.ts
@@ -1,8 +1,25 @@
 import { Footprinter3d } from "../Footprinter3d"
-import { h } from "./h"
-import { render, type RenderResult } from "./render"
+import { h, Fragment, type VNode } from "./h"
+import { render, type RenderResult, type ColoredGeom } from "./render"
+export * from "./convertCSGToThreeGeom"
+
+export { h, Fragment }
+export type { VNode, RenderResult, ColoredGeom }
 
 export function getJscadModelForFootprint(footprint: string): RenderResult {
   const vnode = h(Footprinter3d, { footprint })
   return render(vnode)
+}
+
+export function createJSCADRenderer(_jscad: any) {
+  function createJSCADRoot(container: ColoredGeom[]) {
+    return {
+      render(element: VNode) {
+        const { geometries } = render(element)
+        container.splice(0, container.length, ...geometries)
+      },
+    }
+  }
+
+  return { createJSCADRoot }
 }
