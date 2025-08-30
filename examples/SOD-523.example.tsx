4cf88c6f965d5afba18f5e3d19bf00cc290c3c8c
Mustafa Mulla <97171641+MustafaMulla29@users.noreply.github.com>
2025-08-30 10:29:42 +0000

    Added 3d for sod523

---
+++ examples/SOD-523.example.tsx
@@ -0,0 +1,12 @@
+import { JsCadView } from "jscad-fiber"
+import { SOD523 } from "../lib/sod-523"
+import { ExtrudedPads } from "../lib/ExtrudedPads"
+
+export default () => {
+  return (
+    <JsCadView zAxisUp showGrid>
+      <SOD523 fullWidth={1.6} fullLength={0.8} />
+      <ExtrudedPads footprint="sod523" />
+    </JsCadView>
+  )
+}
---
+++ lib/sod-523.tsx
@@ -0,0 +1,61 @@
+import { ChipBody } from "./ChipBody"
+import { SmdChipLead } from "./SmdChipLead"
+
+export const SOD523 = ({ fullWidth = 1.6, fullLength = 0.8 }) => {
+  const bodyWidth = 1.2
+  const bodyLength = 0.8
+  const bodyHeight = 0.6
+  const leadWidth = 0.3
+  const leadThickness = 0.1
+  const leadHeight = 0.3
+  const padContactLength = 0.2
+  const leadYOffset = leadHeight / 1 - 0.2
+  const bodyYOffset = leadHeight / 2 - 0.2
+
+  const bodyDistance = (fullWidth - bodyWidth) / 2
+
+  return (
+    <>
+      {/* Lead on the left side */}
+      <SmdChipLead
+        key={1}
+        position={{
+          x: -fullWidth / 2 + leadWidth / 2,
+          y: leadYOffset,
+          z: 0,
+        }}
+        width={leadWidth}
+        thickness={leadThickness}
+        padContactLength={padContactLength}
+        bodyDistance={bodyDistance}
+        height={leadHeight}
+      />
+
+      {/* Lead on the right side */}
+      <SmdChipLead
+        key={2}
+        rotation={Math.PI}
+        position={{
+          x: fullWidth / 2 - leadWidth / 2,
+          y: leadYOffset,
+          z: 0,
+        }}
+        width={leadWidth}
+        thickness={leadThickness}
+        padContactLength={padContactLength}
+        bodyDistance={bodyDistance}
+        height={leadHeight}
+      />
+
+      {/* Chip Body */}
+      <ChipBody
+        center={{ x: 0, y: bodyYOffset, z: 0 }}
+        width={bodyWidth}
+        length={bodyLength}
+        height={bodyHeight}
+      />
+    </>
+  )
+}
+
+export default SOD523
