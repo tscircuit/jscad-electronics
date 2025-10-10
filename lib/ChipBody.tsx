5a429cb857a561b8f6a4e366ed82a3f63db251c4
Manish chaudhary <125847751+techmannih@users.noreply.github.com>
2025-10-10 22:29:11 +0530

    Add tapered hull model to chip bodies (#123)

--- lib/ChipBody.tsx
+++ lib/ChipBody.tsx
@@ -1,5 +1,13 @@
-import { Colorize, RoundedCuboid, Translate } from "jscad-fiber"
-
+import {
+  Colorize,
+  Cuboid,
+  Cylinder,
+  Hull,
+  Rotate,
+  Subtract,
+  Translate,
+  Union,
+} from "jscad-fiber"
 export interface ChipBodyProps {
   width: number
   length: number
@@ -15,12 +23,46 @@ export const ChipBody = ({
   height,
   heightAboveSurface = 0.15,
 }: ChipBodyProps) => {
+  const straightHeight = height * 0.5
+  const taperHeight = height - straightHeight
+  const taperInset = Math.min(width, length) * 0.12
+  const faceWidth = Math.max(width - taperInset, width * 0.75)
+  const faceLength = Math.max(length - taperInset, length * 0.75)
+  const notchRadius = Math.min(width, length) * 0.12
+  const notchCenterZ = height - notchRadius * 0.6
+  const notchCenterY = length / 2 - notchRadius * 0.25
+  const notchLength = faceLength / 8
+  const notchWidth = height / 4
   // TODO the bodies flex a bit outward IRL
   return (
     <Colorize color="#555">
       <Translate offset={center}>
-        <Translate offset={{ x: 0, y: 0, z: height / 2 + heightAboveSurface }}>
-          <RoundedCuboid roundRadius={0.2} size={[width, length, height]} />
+        <Translate offset={{ x: 0, y: 0, z: heightAboveSurface }}>
+          <Subtract>
+            <Union>
+              <Hull>
+                <Translate z={0.005}>
+                  <Cuboid size={[faceWidth, faceLength, 0.01]} />
+                </Translate>
+                <Translate z={straightHeight}>
+                  <Cuboid size={[width, length, 0.01]} />
+                </Translate>
+              </Hull>
+              <Hull>
+                <Translate z={straightHeight}>
+                  <Cuboid size={[width, length, 0.01]} />
+                </Translate>
+                <Translate z={straightHeight + taperHeight}>
+                  <Cuboid size={[faceWidth, faceLength, 0.01]} />
+                </Translate>
+              </Hull>
+            </Union>
+            <Translate offset={{ x: 0, y: notchCenterY, z: height }}>
+              <Rotate rotation={[0, 0, 0]}>
+                <Cylinder radius={notchLength} height={notchWidth} />
+              </Rotate>
+            </Translate>
+          </Subtract>
         </Translate>
       </Translate>
     </Colorize>
