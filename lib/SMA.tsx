1fb566c98b96af656eb0ea957b2d47684e788c02
Excellencedev <ademiluyisuccessandexcellence@gmail.com>
2025-10-17 04:38:53 +0100

    WIP

--- lib/SMA.tsx
+++ lib/SMA.tsx
@@ -1,105 +1,66 @@
-import { Cuboid, Cylinder, Subtract, Union } from "jscad-fiber"
+import { ChipBody } from "./ChipBody"
+import { SmdChipLead } from "./SmdChipLead"
 import type { FC } from "react"
 
 interface SMAProps {
-  bodyLength?: number
-  bodyDiameter?: number
-  threadLength?: number
-  threadDiameter?: number
-  pinLength?: number
-  pinDiameter?: number
-  legWidth?: number
-  legHeight?: number
-  legDepth?: number
+  fullWidth?: number
+  fullLength?: number
 }
 
-export const SMA: FC<SMAProps> = ({
-  bodyLength = 10,
-  bodyDiameter = 6,
-  threadLength = 5,
-  threadDiameter = 5,
-  pinLength = 17,
-  pinDiameter = 1,
-  legWidth = 1,
-  legHeight = 1,
-  legDepth = 2,
-}) => {
-  const body = (
-    <Cylinder
-      height={bodyLength}
-      radius={bodyDiameter / 2}
-      center={{ x: 0, y: 0, z: bodyLength / 2 }}
-    />
-  )
+export const SMA: FC<SMAProps> = ({ fullWidth = 5.6, fullLength = 2.8 }) => {
+  const bodyWidth = 4.3
+  const bodyLength = 2.6
+  const bodyHeight = 2.2
+  const leadWidth = 1.5
+  const leadThickness = 0.2
+  const leadHeight = 1.0
+  const padContactLength = 0.8
+  const padThickness = leadThickness / 2
 
-  const thread = (
-    <Cylinder
-      height={threadLength}
-      radius={threadDiameter / 2}
-      center={{ x: 0, y: 0, z: bodyLength + threadLength / 2 }}
-    />
-  )
-
-  const pin = (
-    <Cylinder
-      height={pinLength}
-      radius={pinDiameter / 2}
-      center={{ x: 0, y: 0, z: 7.5 }}
-    />
-  )
-
-  const leg = (
-    <Cuboid
-      size={[legWidth, legDepth, legHeight]}
-      center={{ x: 0, y: 0, z: 0 }}
-    />
-  )
-
-  const legs = (
-    <>
-      <Union>
-        <Cuboid
-          size={[legWidth, legDepth, legHeight]}
-          center={{
-            x: bodyDiameter / 2 + legWidth / 2,
-            y: 0,
-            z: legHeight / 2,
-          }}
-        />
-        <Cuboid
-          size={[legWidth, legDepth, legHeight]}
-          center={{
-            x: -(bodyDiameter / 2 + legWidth / 2),
-            y: 0,
-            z: legHeight / 2,
-          }}
-        />
-        <Cuboid
-          size={[legDepth, legWidth, legHeight]}
-          center={{
-            x: 0,
-            y: bodyDiameter / 2 + legWidth / 2,
-            z: legHeight / 2,
-          }}
-        />
-        <Cuboid
-          size={[legDepth, legWidth, legHeight]}
-          center={{
-            x: 0,
-            y: -(bodyDiameter / 2 + legWidth / 2),
-            z: legHeight / 2,
-          }}
-        />
-      </Union>
-    </>
-  )
+  const bodyDistance = (fullWidth - bodyWidth) / 2
 
   return (
-    <Union>
-      {body}
-      {thread}
-      {pin}
-      {legs}
-    </Union>
+    <>
+      {/* Lead on the left side */}
+      <SmdChipLead
+        key={1}
+        position={{
+          x: -fullWidth / 2 + leadWidth / 2,
+          y: 0,
+          z: padThickness,
+        }}
+        width={leadWidth}
+        thickness={leadThickness}
+        padContactLength={padContactLength}
+        bodyDistance={bodyDistance}
+        height={leadHeight}
+      />
+
+      {/* Lead on the right side */}
+      <SmdChipLead
+        key={2}
+        rotation={Math.PI}
+        position={{
+          x: fullWidth / 2 - leadWidth / 2,
+          y: 0,
+          z: padThickness,
+        }}
+        width={leadWidth}
+        thickness={leadThickness}
+        padContactLength={padContactLength}
+        bodyDistance={bodyDistance}
+        height={leadHeight}
+      />
+
+      {/* Chip Body */}
+      <ChipBody
+        center={{ x: 0, y: 0, z: 0 }}
+        width={bodyWidth}
+        length={bodyLength}
+        height={bodyHeight}
+      />
+    </>
   )
 }
+
+export default SMA
