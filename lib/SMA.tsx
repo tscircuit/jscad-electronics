9a0136cafd81493d464c5179eb6ee08416c49b13
Excellencedev <ademiluyisuccessandexcellence@gmail.com>
2025-10-17 14:46:25 +0100

    WIP

--- lib/SMA.tsx
+++ lib/SMA.tsx
@@ -1,46 +1,48 @@
 import { ChipBody } from "./ChipBody"
-import { SmdChipLead } from "./SmdChipLead"
 import type { FC } from "react"
-import { Cuboid, Colorize } from "jscad-fiber"
+import { Cuboid, Colorize, Union } from "jscad-fiber"
 
 export const SMA: FC = () => {
   // Dimensions from diodes.com SMA.pdf datasheet
   const bodyLength = 4.25 // H
   const bodyWidth = 4.3 // B
   const bodyHeight = 2.14 // C
-  const overallLength = 5.1 // D
   const leadWidth = 1.45 // A
-  const leadLength = (overallLength - bodyLength) / 2
+  const leadContactLength = 1.14 // G
+  const leadThickness = 0.2
+
+  const Lead = ({ position }: { position: "positive" | "negative" }) => {
+    const x = position === "positive" ? 1 : -1
+    return (
+      <Colorize color="#ccc">
+        <Union>
+          {/* Horizontal part of the lead (pad) */}
+          <Cuboid
+            size={[leadContactLength, leadWidth, leadThickness]}
+            center={{
+              x: x * (bodyLength / 2 + leadContactLength / 2),
+              y: 0,
+              z: leadThickness / 2,
+            }}
+          />
+          {/* Vertical part of the lead */}
+          <Cuboid
+            size={[leadThickness, leadWidth, bodyHeight * 0.6]}
+            center={{
+              x: x * (bodyLength / 2 - leadThickness / 2),
+              y: 0,
+              z: (bodyHeight * 0.6) / 2,
+            }}
+          />
+        </Union>
+      </Colorize>
+    )
+  }
 
   return (
     <>
-      <SmdChipLead
-        key={1}
-        position={{
-          x: -bodyLength / 2 - leadLength / 2,
-          y: 0,
-          z: 0.1,
-        }}
-        width={leadWidth}
-        thickness={0.2}
-        padContactLength={leadLength}
-        bodyDistance={0}
-        height={bodyHeight / 2}
-      />
-      <SmdChipLead
-        key={2}
-        rotation={Math.PI}
-        position={{
-          x: bodyLength / 2 + leadLength / 2,
-          y: 0,
-          z: 0.1,
-        }}
-        width={leadWidth}
-        thickness={0.2}
-        padContactLength={leadLength}
-        bodyDistance={0}
-        height={bodyHeight / 2}
-      />
+      <Lead position="positive" />
+      <Lead position="negative" />
 
       <ChipBody
         center={{ x: 0, y: 0, z: 0 }}
@@ -52,8 +54,8 @@ export const SMA: FC = () => {
       {/* Polarity Marking */}
       <Colorize color="#FFFFFF">
         <Cuboid
-          size={[bodyWidth * 0.9, 0.4, 0.1]}
-          center={{ x: 0, y: bodyLength / 2 - 0.2, z: bodyHeight - 0.05 }}
+          size={[0.4, bodyWidth * 0.9, 0.1]}
+          center={{ x: bodyLength / 2 - 0.2, y: 0, z: bodyHeight - 0.05 }}
         />
       </Colorize>
     </>
