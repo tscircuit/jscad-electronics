6134cd8aa42582e60f5b1fcaee3adbcefff3010b
Rahul Nailoo <rahulsinghnailoo@gmail.com>
2025-10-21 08:16:48 +0530

    Add support for SMB model (#151)

---
+++ examples/SMB.example.tsx
@@ -0,0 +1,12 @@
+import { ExtrudedPads } from "../lib/ExtrudedPads"
+import { JsCadView } from "jscad-fiber"
+import { SMB } from "../lib/SMB"
+
+export default () => {
+  return (
+    <JsCadView zAxisUp showGrid>
+      <SMB />
+      <ExtrudedPads footprint="smb" />
+    </JsCadView>
+  )
+}
---
+++ tests/snapshots/smb.test.ts
@@ -0,0 +1,8 @@
+import { test, expect } from "bun:test"
+import "../fixtures/png-matcher"
+import { renderFootprint } from "../helpers/render-footprint"
+
+test("SMB diode", async () => {
+  const pngBuffer = await renderFootprint("smb")
+  await expect(pngBuffer).toMatchPngSnapshot(import.meta.path)
+})
