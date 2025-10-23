70010bf48f7f9f1262d3c2f163bb5b0b1d8c74a3
Raghav Arora <rkinger2004@gmail.com>
2025-10-23 20:02:10 +0530

    Add support for SMC diode package (#153)

---
+++ examples/SMC.example.tsx
@@ -0,0 +1,12 @@
+import { ExtrudedPads } from "../lib/ExtrudedPads"
+import { JsCadView } from "jscad-fiber"
+import { SMC } from "../lib/SMC"
+
+export default () => {
+  return (
+    <JsCadView zAxisUp showGrid>
+      <SMC />
+      <ExtrudedPads footprint="smc" />
+    </JsCadView>
+  )
+}
