2fe2de2c4814d7db47da2788d998867c8ddbcd09
nailoo <rahulsinghnailoo@gmail.com>
2025-10-27 07:37:47 +0530

    add snapshot

--- lib/sod-123F.tsx
+++ lib/sod-123F.tsx
@@ -9,28 +9,27 @@ export const SOD123F = () => {
   const padLength = 1
   const padThickness = 0.12
 
-  const bodyWidth = fullWidth
   const leftPadCenterX = -1.3
   const rightPadCenterX = 1.3
 
   const taperOffset = 0.2
   const straightHeight = bodyHeight * 0.5
 
-  const Body = (
+  const body = (
     <Colorize color="#222">
       <Union>
         <Translate z={straightHeight / 2}>
-          <Cuboid size={[bodyWidth, bodyLength, straightHeight]} />
+          <Cuboid size={[fullWidth, bodyLength, straightHeight]} />
         </Translate>
 
         <Hull>
           <Translate z={straightHeight}>
-            <Cuboid size={[bodyWidth, bodyLength, 0.01]} />
+            <Cuboid size={[fullWidth, bodyLength, 0.01]} />
           </Translate>
 
           <Translate z={bodyHeight}>
             <Cuboid
-              size={[bodyWidth - taperOffset, bodyLength - taperOffset, 0.01]}
+              size={[fullWidth - taperOffset, bodyLength - taperOffset, 0.01]}
             />
           </Translate>
         </Hull>
@@ -51,7 +50,7 @@ export const SOD123F = () => {
         center={[rightPadCenterX, 0, padThickness / 2]}
       />
 
-      {Body}
+      {body}
     </>
   )
 }
