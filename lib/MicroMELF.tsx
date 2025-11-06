ac8305f4d884fd4a0c645167a56e0ec0bf467de2
rushabhcodes <mail@rushabh.dev>
2025-11-06 15:08:13 +0530

    Refactor MicroMELF component to use RoundedCuboid and add cathode identification rendering

--- lib/MicroMELF.tsx
+++ lib/MicroMELF.tsx
@@ -1,10 +1,17 @@
-import { Colorize, Cuboid, RoundedCylinder, Rotate } from "jscad-fiber"
+import {
+  Colorize,
+  Cuboid,
+  RoundedCylinder,
+  Rotate,
+  RoundedCuboid,
+} from "jscad-fiber"
 
 export interface MicroMELFProps {
   bodyLength?: number
   bodyDiameter?: number
   color?: string
   contactColor?: string
+  cathodeIdentification?: string
 }
 
 export const MicroMELF = ({
@@ -12,18 +19,29 @@ export const MicroMELF = ({
   bodyDiameter = 1.1,
   color = "#3a3a3aff",
   contactColor = "#c6c6c6",
+  cathodeIdentification = "#111",
 }: MicroMELFProps) => {
-  const padLength = 0.20
+  const padLength = 0.2
 
   return (
     <>
       <Colorize color={color}>
         <Rotate rotation={[0, "90deg", 0]}>
-          <RoundedCylinder
-            height={bodyLength}
-            radius={bodyDiameter / 2}
-            roundRadius={padLength / 2}
+          <RoundedCuboid
+            size={[bodyDiameter, bodyDiameter, bodyLength - padLength / 2]}
+            roundRadius={padLength}
             center={[-bodyDiameter / 2, 0, 0]}
+          />
+        </Rotate>
+      </Colorize>
+      {/* Cathode identification: a dark stripe near the left end of the body */}
+
+      <Colorize color={cathodeIdentification}>
+        <Rotate rotation={[0, "90deg", 0]}>
+          <RoundedCuboid
+            size={[bodyDiameter * 1.01, bodyDiameter * 1.01, bodyLength / 3]}
+            roundRadius={padLength}
+            center={[-bodyDiameter / 2, 0, -bodyLength / 4]}
           />
         </Rotate>
       </Colorize>
@@ -33,7 +51,7 @@ export const MicroMELF = ({
           <RoundedCylinder
             height={padLength}
             radius={bodyDiameter / 2}
-            roundRadius={padLength/3}
+            roundRadius={padLength / 3}
             center={[-bodyDiameter / 2, 0, -bodyLength / 2]}
           />
         </Rotate>
@@ -44,7 +62,7 @@ export const MicroMELF = ({
           <RoundedCylinder
             height={padLength}
             radius={bodyDiameter / 2}
-            roundRadius={padLength/3}
+            roundRadius={padLength / 3}
             center={[-bodyDiameter / 2, 0, bodyLength / 2]}
           />
         </Rotate>
