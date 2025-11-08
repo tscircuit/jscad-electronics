b1442e2ad5c99ee3b0674a68645f65d88aae7339
nailoo <rahulsinghnailoo@gmail.com>
2025-11-08 22:01:37 +0530

    up

--- lib/SOT-723.tsx
+++ lib/SOT-723.tsx
@@ -2,11 +2,11 @@ import { Cuboid, Translate, Rotate, Colorize } from "jscad-fiber"
 
 const getCcwSot723Coords = (pn: number) => {
   if (pn === 1) {
-    return { x: 0, y: 0 }
+    return { x: 0.55, y: 0 }
   } else if (pn === 2) {
-    return { x: 1, y: -0.4 }
+    return { x: -0.55, y: 0.4 }
   } else {
-    return { x: 1, y: 0.4 }
+    return { x: -0.55, y: -0.4 }
   }
 }
 
@@ -14,15 +14,15 @@ export const SOT723 = () => {
   const bodyWidth = 0.85
   const bodyLength = 1.2
   const bodyHeight = 0.5
-  const leadWidth = 0.22
-  const leadLength = 0.2
+  const leadWidth = 0.2
+  const leadLength = 0.3
   const leadHeight = 0.1
   const centerLeadWidth = 0.25
 
   return (
     <>
       <Rotate rotation={[45 * Math.PI, 0, 0]}>
-        <Translate center={[0.5, 0, -0.25]}>
+        <Translate center={[0, 0, -0.25]}>
           <Colorize color="grey">
             <Cuboid size={[bodyWidth, bodyLength, bodyHeight]} />
           </Colorize>
