c4d7c8123ef78cbdbe563d4b31837373207149c2
rushabhcodes <mail@rushabh.dev>
2025-11-08 20:31:07 +0530

    Refactor SOD882 component for improved code readability and consistency

--- lib/SOD882.tsx
+++ lib/SOD882.tsx
@@ -9,9 +9,9 @@ export const SOD882 = () => {
   const padLength = 0.26
   const padThickness = 0.12
 
-  const bodyWidth =  0.58
-  const leftPadCenterX = -pitch/2
-  const rightPadCenterX = pitch/2
+  const bodyWidth = 0.58
+  const leftPadCenterX = -pitch / 2
+  const rightPadCenterX = pitch / 2
 
   return (
     <>
@@ -31,31 +31,31 @@ export const SOD882 = () => {
 
       {/* Body */}
       <Colorize color="#222">
-          <Translate z={bodyHeight / 2 + 0.02}>
-            <Cuboid size={[bodyLength, bodyWidth, bodyHeight]} />
-          </Translate>
+        <Translate z={bodyHeight / 2 + 0.02}>
+          <Cuboid size={[bodyLength, bodyWidth, bodyHeight]} />
+        </Translate>
       </Colorize>
 
       <Cuboid
         color="#ccc"
-        size={[bodyLength + 0.001, padLength/2, padLength/4]}
-        center={[0, padLength/2, bodyHeight / 4 ]}
+        size={[bodyLength + 0.001, padLength / 2, padLength / 4]}
+        center={[0, padLength / 2, bodyHeight / 4]}
       />
-       <Cuboid
-        color="#ccc"
-        size={[bodyLength + 0.001, padLength/2, padLength/4]}
-        center={[0, -padLength/2, bodyHeight / 4 ]}
-      />
-      
       <Cuboid
         color="#ccc"
-        size={[padLength/1.5, bodyWidth+0.001, padLength/4]}
-        center={[pitch/2, 0, bodyHeight / 4 ]}
+        size={[bodyLength + 0.001, padLength / 2, padLength / 4]}
+        center={[0, -padLength / 2, bodyHeight / 4]}
       />
-       <Cuboid
+
+      <Cuboid
         color="#ccc"
-        size={[padLength/1.5, bodyWidth+0.001, padLength/4]}
-        center={[-pitch/2, 0, bodyHeight / 4 ]}
+        size={[padLength / 1.5, bodyWidth + 0.001, padLength / 4]}
+        center={[pitch / 2, 0, bodyHeight / 4]}
+      />
+      <Cuboid
+        color="#ccc"
+        size={[padLength / 1.5, bodyWidth + 0.001, padLength / 4]}
+        center={[-pitch / 2, 0, bodyHeight / 4]}
       />
     </>
   )
