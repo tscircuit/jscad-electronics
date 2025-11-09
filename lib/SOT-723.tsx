a086c351c425b033eff48e322fcfee172b9bceca
nailoo <rahulsinghnailoo@gmail.com>
2025-11-10 01:47:21 +0530

    fix(sot723): correct body/pad colors & CAD dimensions; add snapshot test; update footprinter

--- lib/SOT-723.tsx
+++ lib/SOT-723.tsx
@@ -1,48 +1,52 @@
-import { Cuboid, Translate, Rotate, Colorize } from "jscad-fiber"
-
-const getCcwSot723Coords = (pn: number) => {
-  if (pn === 1) {
-    return { x: 0, y: 0 }
-  } else if (pn === 2) {
-    return { x: 1, y: -0.4 }
-  } else {
-    return { x: 1, y: 0.4 }
-  }
-}
+import { Cuboid, Translate, Colorize } from "jscad-fiber"
 
 export const SOT723 = () => {
-  const bodyWidth = 0.8
+  // Body dimensions
+  const bodyWidth = 0.85
   const bodyLength = 1.2
-  const bodyHeight = 0.5
-  const leadWidth = 0.32
-  const leadLength = 0.3
-  const leadHeight = 0.1
-  const centerLeadWidth = 0.42
+  const bodyHeight = 0.38
+
+  const padLength = 0.3
+  const padThickness = 0.1
+  const leftPadWidth = 0.2
+  const rightPadWidth = 0.25
+
+  const rightPadCenterX = 0.55
+  const rightPadCenterY = 0
+
+  const leftTopPadCenterX = -0.55
+  const leftTopPadCenterY = 0.4
+
+  const leftBottomPadCenterX = -0.55
+  const leftBottomPadCenterY = -0.4
 
   return (
     <>
-      <Rotate rotation={[45 * Math.PI, 0, 0]}>
-        <Translate center={[0.475, leadHeight / 2, -0.25]}>
-          <Colorize color="grey">
-            <Cuboid size={[bodyWidth, bodyLength, bodyHeight]} />
-          </Colorize>
-        </Translate>
-      </Rotate>
+      {/* Body */}
+      <Colorize color="#222">
+        <Cuboid
+          size={[bodyWidth, bodyLength, bodyHeight]}
+          center={[0, 0, bodyHeight / 2]}
+        />
+      </Colorize>
 
-      {[1, 2, 3].map((pn) => {
-        const { x, y } = getCcwSot723Coords(pn)
-        return (
-          <Translate key={`lead-${pn}`} center={[x, y, 0.05]}>
-            <Cuboid
-              size={[
-                leadLength,
-                pn === 1 ? centerLeadWidth : leadWidth,
-                leadHeight,
-              ]}
-            />
-          </Translate>
-        )
-      })}
+      <Cuboid
+        color="#ccc"
+        size={[padLength, rightPadWidth, padThickness]}
+        center={[rightPadCenterX, rightPadCenterY, padThickness / 2]}
+      />
+
+      <Cuboid
+        color="#ccc"
+        size={[padLength, leftPadWidth, padThickness]}
+        center={[leftTopPadCenterX, leftTopPadCenterY, padThickness / 2]}
+      />
+
+      <Cuboid
+        color="#ccc"
+        size={[padLength, leftPadWidth, padThickness]}
+        center={[leftBottomPadCenterX, leftBottomPadCenterY, padThickness / 2]}
+      />
     </>
   )
 }
