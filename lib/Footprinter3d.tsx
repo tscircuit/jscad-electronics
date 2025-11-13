016886aa4a19ec832406eb2e2b105401ac49dbba
Rushabh Patil <rushabhpatil.dev@gmail.com>
2025-11-14 01:05:12 +0530

    Implement  SOT457 with example and snapshot test (#217)
    * Implement  SOT457 with example and snapshot test
    
    * feat(SOT457): add notch position to component
    
    * fix(SOT457): adjust lead positions and update notch parameters
    
    * fix(SOT457): format notchPosition z-coordinate for consistency
    
    ---------
    
    Co-authored-by: rushabhcodes <mail@rushabh.dev>

--- lib/Footprinter3d.tsx
+++ lib/Footprinter3d.tsx
@@ -40,6 +40,7 @@ import { MINIMELF } from "./MINIMELF"
 import { MELF } from "./MELF"
 import { MS012 } from "./ms012"
 import { TO220 } from "./TO220"
+import { SOT457 } from "./SOT-457"
 import { TO92 } from "./TO92"
 import SOT363 from "./SOT-363"
 
@@ -192,6 +193,8 @@ export const Footprinter3d = ({ footprint }: { footprint: string }) => {
     }
     case "sot235":
       return <SOT235 />
+    case "sot457":
+      return <SOT457 />
     case "sot223":
       return <SOT223 />
     case "sot323":
---
+++ lib/SOT-457.tsx
@@ -0,0 +1,123 @@
+import { ChipBody } from "./ChipBody"
+import { SmdChipLead } from "./SmdChipLead"
+
+export const SOT457 = () => {
+  const fullWidth = 2.8
+  const bodyWidth = 1.6
+  const bodyLength = 2.9
+  const bodyHeight = 1.2
+  const leadWidth = 0.4
+  const leadThickness = 0.15
+  const leadHeight = 0.95
+  const padContactLength = 0.5
+  const padPitch = 0.95
+
+  // Increase the bodyDistance to extend leads further out
+  const extendedBodyDistance = fullWidth - bodyWidth
+
+  return (
+    <>
+      {/* Leads on the right side (pins 1 and 2) */}
+      <SmdChipLead
+        key={1}
+        rotation={Math.PI}
+        position={{
+          x: fullWidth / 2 + extendedBodyDistance / 4,
+          y: -padPitch,
+          z: leadThickness / 2,
+        }}
+        width={leadWidth}
+        thickness={leadThickness}
+        padContactLength={padContactLength}
+        bodyDistance={extendedBodyDistance}
+        height={leadHeight}
+      />
+      <SmdChipLead
+        key={2}
+        rotation={Math.PI}
+        position={{
+          x: fullWidth / 2 + extendedBodyDistance / 4,
+          y: 0,
+          z: leadThickness / 2,
+        }}
+        width={leadWidth}
+        thickness={leadThickness}
+        padContactLength={padContactLength}
+        bodyDistance={extendedBodyDistance}
+        height={leadHeight}
+      />
+
+      <SmdChipLead
+        key={3}
+        rotation={Math.PI}
+        position={{
+          x: fullWidth / 2 + extendedBodyDistance / 4,
+          y: padPitch,
+          z: leadThickness / 2,
+        }}
+        width={leadWidth}
+        thickness={leadThickness}
+        padContactLength={padContactLength}
+        bodyDistance={extendedBodyDistance}
+        height={leadHeight}
+      />
+
+      {/* Lead on the left side (pin 3) */}
+      <SmdChipLead
+        key={3}
+        position={{
+          x: -fullWidth / 2 - extendedBodyDistance / 4,
+          y: 0,
+          z: leadThickness / 2,
+        }}
+        width={leadWidth}
+        thickness={leadThickness}
+        padContactLength={padContactLength}
+        bodyDistance={extendedBodyDistance}
+        height={leadHeight}
+      />
+      <SmdChipLead
+        key={1}
+        position={{
+          x: -fullWidth / 2 - extendedBodyDistance / 4,
+          y: -padPitch,
+          z: leadThickness / 2,
+        }}
+        width={leadWidth}
+        thickness={leadThickness}
+        padContactLength={padContactLength}
+        bodyDistance={extendedBodyDistance}
+        height={leadHeight}
+      />
+      <SmdChipLead
+        key={2}
+        position={{
+          x: -fullWidth / 2 - extendedBodyDistance / 4,
+          y: padPitch,
+          z: leadThickness / 2,
+        }}
+        width={leadWidth}
+        thickness={leadThickness}
+        padContactLength={padContactLength}
+        bodyDistance={extendedBodyDistance}
+        height={leadHeight}
+      />
+      {/* Chip Body */}
+      <ChipBody
+        center={{ x: 0, y: 0, z: 0 }}
+        width={bodyWidth}
+        length={bodyLength}
+        height={bodyHeight}
+        straightHeightRatio={0.6}
+        notchPosition={{
+          x: bodyWidth / 2 - 0.4,
+          y: bodyHeight / 2 + 0.4,
+          z: bodyHeight + 0.05,
+        }}
+        notchRadius={0.1}
+      />
+    </>
+  )
+}
+
+export default SOT457
