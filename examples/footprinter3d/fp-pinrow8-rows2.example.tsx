aca92d58495f63d4eb05e77ef824d2c5250d930e
Rahul Nailoo <rahulsinghnailoo@gmail.com>
2025-12-09 00:10:32 +0530

    update name

--- examples/footprinter3d/fp-pinrow8-rows2.example.tsx
+++ examples/footprinter3d/fp-pinrow8-rows2.example.tsx
@@ -2,7 +2,7 @@ import { JsCadView } from "jscad-fiber"
 import { Footprinter3d } from "lib/Footprinter3d"
 import { ExtrudedPads } from "lib/ExtrudedPads"
 
-const footprint = "pinrow4_rows2_id01mm_p2.54mm_od01.6mm"
+const footprint = "pinrow8_rows2_id01mm_p2.54mm_od01.6mm"
 export default () => {
   return (
     <JsCadView zAxisUp showGrid>
