c53e67760648e70ea4ea0424fdf9b4e48c643135
rushabhcodes <mail@rushabh.dev>
2026-01-01 23:03:21 +0530

    Refactor AxialCapacitor component for improved readability

--- lib/AxialCapacitor.tsx
+++ lib/AxialCapacitor.tsx
@@ -14,21 +14,21 @@ export const AxialCapacitor = ({ pitch = 10 }: AxialCapacitorProps) => {
         radius={0.5}
         center={[-pitch / 2, 0, heightToCenterOfCapacitor]}
       />
-      <Translate x={-2.5  } y={0} z={heightToCenterOfCapacitor}>
-      <Rotate rotation={[0,Math.PI/2, 0]}>
-      <Cylinder
-        height={pitch}
-        radius={0.5}
-        center={[0, 0, heightToCenterOfCapacitor]}
-      />
-      <Colorize color="#d2b48c">
-      <Cylinder
-        height={5}
-        radius={1.3}
-        center={[0, 0, heightToCenterOfCapacitor]}
-      />
-      </Colorize> 
-      </Rotate>
+      <Translate x={-2.5} y={0} z={heightToCenterOfCapacitor}>
+        <Rotate rotation={[0, Math.PI / 2, 0]}>
+          <Cylinder
+            height={pitch}
+            radius={0.5}
+            center={[0, 0, heightToCenterOfCapacitor]}
+          />
+          <Colorize color="#d2b48c">
+            <Cylinder
+              height={5}
+              radius={1.3}
+              center={[0, 0, heightToCenterOfCapacitor]}
+            />
+          </Colorize>
+        </Rotate>
       </Translate>
       <Cylinder height={4} radius={0.5} center={[pitch / 2, 0, 0.5]} />
       <Sphere radius={0.5} center={[pitch / 2, 0, heightToCenterOfCapacitor]} />
