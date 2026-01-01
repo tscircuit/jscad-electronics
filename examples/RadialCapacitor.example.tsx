e585383a3b1a5e35bec24414490997e7732506ba
Rushabh Patil <mail@rushabh.dev>
2026-01-01 15:59:41 +0530

    Add RadialCapacitor component and update footprinter dependency (#248)

--- examples/RadialCapacitor.example.tsx
+++ examples/RadialCapacitor.example.tsx
@@ -1,12 +1,12 @@
 import { JsCadView, Translate, Union } from "jscad-fiber"
-import { AxialCapacitor } from "../lib/AxialCapacitor"
+import { RadialCapacitor } from "../lib/RadialCapacitor"
 import { ExtrudedPads } from "../lib/ExtrudedPads"
 
 export default () => {
   return (
     <JsCadView zAxisUp showGrid>
-      <AxialCapacitor pitch={14} variant="vertical" />
-      <ExtrudedPads footprint="axial_p14mm" />
+      <RadialCapacitor pitch={14} variant="vertical" />
+      <ExtrudedPads footprint="radial_p14mm_id1mm" />
     </JsCadView>
   )
 }
