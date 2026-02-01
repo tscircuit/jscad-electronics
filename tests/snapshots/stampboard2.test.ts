d375372d36d43c4a2364f2fdb879d2010b49a6e7
rushabhcodes <mail@rushabh.dev>
2026-02-01 23:03:01 +0530

    Remove unnecessary blank line in stampboard2 test file

--- tests/snapshots/stampboard2.test.ts
+++ tests/snapshots/stampboard2.test.ts
@@ -2,7 +2,6 @@ import { test, expect } from "bun:test"
 import "../fixtures/png-matcher"
 import { renderFootprint } from "../helpers/render-footprint"
 
-
 test("stampboard receiver with inner holes", async () => {
   const pngBuffer = await renderFootprint(
     "stampreceiver_left10_right20_bottom6_top3_w21mm_innerhole",
