faa2a59fb638d1fc09e86cb8396d1f8b50aafefe
seveibar <seveibar@gmail.com>
2026-09-13 21:54:02 -0700

    test: exercise supported reversed crystal orientation

--- tests/snapshots/crystal2-4.test.ts
+++ tests/snapshots/crystal2-4.test.ts
@@ -4,7 +4,7 @@ import { renderContactSheet } from "../helpers/render-contact-sheet"
 test("NX3225GD", async () => {
   await expect(
     await renderContactSheet(
-      "smdpads2_p1.8999mm_pw1.5mm_ph2.7mm_pin1location(leftside,top)_crystal2NX3225GD",
+      "smdpads2_p1.8999mm_pw1.5mm_ph2.7mm_pin1location(rightside,top)_crystal2NX3225GD",
     ),
   ).toMatchPngSnapshot(import.meta.path, "crystal2-NX3225GD")
 }, 30000)
