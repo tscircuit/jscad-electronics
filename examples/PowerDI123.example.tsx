5dc15aa33895dab848e101659726bd43cc7d0859
seveibar <seveibar@gmail.com>
2026-09-14 11:36:44 -0700

    Fit fixture camera to physical component bounds

--- examples/PowerDI123.example.tsx
+++ examples/PowerDI123.example.tsx
@@ -1,10 +1,12 @@
-import { JsCadView } from "jscad-fiber"
+import { ComponentPreview } from "./utils/ComponentPreview"
 import { PowerDI123 } from "../lib/PowerDI123"
 import { ExtrudedPads } from "../lib/ExtrudedPads"
 const footprint = "diode_p2.3749mm_pw1.725mm_ph1.5mm_rounded0"
 export default () => (
-  <JsCadView zAxisUp showGrid>
-    <PowerDI123 footprint={footprint} />
-    <ExtrudedPads footprint={footprint} />
-  </JsCadView>
+  <ComponentPreview>
+    <>
+      <PowerDI123 footprint={footprint} />
+      <ExtrudedPads footprint={footprint} />
+    </>
+  </ComponentPreview>
 )
