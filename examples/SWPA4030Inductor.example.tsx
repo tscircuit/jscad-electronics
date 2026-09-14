850ebd0ca21649eb9dd90c73d0a1a54131487706
seveibar <seveibar@gmail.com>
2026-09-14 11:36:43 -0700

    Fit fixture camera to physical component bounds

--- examples/SWPA4030Inductor.example.tsx
+++ examples/SWPA4030Inductor.example.tsx
@@ -1,11 +1,13 @@
-import { JsCadView } from "jscad-fiber"
+import { ComponentPreview } from "./utils/ComponentPreview"
 import { SWPA4030Inductor } from "../lib/SWPA4030Inductor"
 import { ExtrudedPads } from "../lib/ExtrudedPads"
 
 const footprint = "smdpads2_p3.6002mm_pw1.9mm_ph3.7mm"
 export default () => (
-  <JsCadView zAxisUp showGrid>
-    <SWPA4030Inductor footprint={footprint} />
-    <ExtrudedPads footprint={footprint} />
-  </JsCadView>
+  <ComponentPreview>
+    <>
+      <SWPA4030Inductor footprint={footprint} />
+      <ExtrudedPads footprint={footprint} />
+    </>
+  </ComponentPreview>
 )
