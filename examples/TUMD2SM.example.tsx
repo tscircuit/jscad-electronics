0c4182594b6189f45f20de5ba5ddbf8a0eb30109
seveibar <seveibar@gmail.com>
2026-09-14 11:36:44 -0700

    Fit fixture camera to physical component bounds

--- examples/TUMD2SM.example.tsx
+++ examples/TUMD2SM.example.tsx
@@ -1,10 +1,12 @@
-import { JsCadView } from "jscad-fiber"
+import { ComponentPreview } from "./utils/ComponentPreview"
 import { TUMD2SM } from "../lib/TUMD2SM"
 import { ExtrudedPads } from "../lib/ExtrudedPads"
 const footprint = "smdpads2_p2.1001mm_pw0.8mm_ph1.1mm"
 export default () => (
-  <JsCadView zAxisUp showGrid>
-    <TUMD2SM footprint={footprint} />
-    <ExtrudedPads footprint={footprint} />
-  </JsCadView>
+  <ComponentPreview>
+    <>
+      <TUMD2SM footprint={footprint} />
+      <ExtrudedPads footprint={footprint} />
+    </>
+  </ComponentPreview>
 )
