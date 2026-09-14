5d0566a421544687f5e03952938076138ace14ee
seveibar <seveibar@gmail.com>
2026-09-14 11:36:43 -0700

    Fit fixture camera to physical component bounds

--- examples/X1SON2.example.tsx
+++ examples/X1SON2.example.tsx
@@ -1,10 +1,12 @@
-import { JsCadView } from "jscad-fiber"
+import { ComponentPreview } from "./utils/ComponentPreview"
 import { X1SON2 } from "../lib/X1SON2"
 import { ExtrudedPads } from "../lib/ExtrudedPads"
 const footprint = "smdpads2_p1mm_pw0.6mm_ph0.6mm"
 export default () => (
-  <JsCadView zAxisUp showGrid>
-    <X1SON2 footprint={footprint} />
-    <ExtrudedPads footprint={footprint} />
-  </JsCadView>
+  <ComponentPreview>
+    <>
+      <X1SON2 footprint={footprint} />
+      <ExtrudedPads footprint={footprint} />
+    </>
+  </ComponentPreview>
 )
