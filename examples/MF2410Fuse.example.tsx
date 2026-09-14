26a51924aae5708eedf6b585101f8c45444492e3
seveibar <seveibar@gmail.com>
2026-09-14 11:36:43 -0700

    Fit fixture camera to physical component bounds

--- examples/MF2410Fuse.example.tsx
+++ examples/MF2410Fuse.example.tsx
@@ -1,11 +1,13 @@
-import { JsCadView } from "jscad-fiber"
+import { ComponentPreview } from "./utils/ComponentPreview"
 import { MF2410Fuse } from "../lib/MF2410Fuse"
 import { ExtrudedPads } from "../lib/ExtrudedPads"
 
 const footprint = "smdpads2_p4.9997mm_pw2mm_ph3.2mm"
 export default () => (
-  <JsCadView zAxisUp showGrid>
-    <MF2410Fuse footprint={footprint} />
-    <ExtrudedPads footprint={footprint} />
-  </JsCadView>
+  <ComponentPreview>
+    <>
+      <MF2410Fuse footprint={footprint} />
+      <ExtrudedPads footprint={footprint} />
+    </>
+  </ComponentPreview>
 )
