8595482527b854eea1373642c8d6c6ba2baba5ef
seveibar <seveibar@gmail.com>
2026-09-14 11:36:43 -0700

    Fit fixture camera to physical component bounds

--- examples/BLM41FerriteBead.example.tsx
+++ examples/BLM41FerriteBead.example.tsx
@@ -1,11 +1,13 @@
-import { JsCadView } from "jscad-fiber"
+import { ComponentPreview } from "./utils/ComponentPreview"
 import { BLM41FerriteBead } from "../lib/BLM41FerriteBead"
 import { ExtrudedPads } from "../lib/ExtrudedPads"
 
 const footprint = "smdpads2_p3.8999mm_pw2mm_ph2mm"
 export default () => (
-  <JsCadView zAxisUp showGrid>
-    <BLM41FerriteBead footprint={footprint} />
-    <ExtrudedPads footprint={footprint} />
-  </JsCadView>
+  <ComponentPreview>
+    <>
+      <BLM41FerriteBead footprint={footprint} />
+      <ExtrudedPads footprint={footprint} />
+    </>
+  </ComponentPreview>
 )
